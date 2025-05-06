4d09d28e206b9a9489b6229b1d59f012885aac17
Francisco Carlos Beltran Arsene <fco.carlos@proton.me>
2025-05-06 23:50:17 +0200

    Start 0 And Laravel 12

--- subir/pages/ofertas.php
+++
@@ -1,7 +0,0 @@
-<?php
-cabecera($sql, $cusers, $gene, 14);
-?>
-Apareceran listado de ofertas que pertenezcan al sector y experiencia del empelador, con mensaje directo a la empresa!
-<?php
-pie();
-?>
